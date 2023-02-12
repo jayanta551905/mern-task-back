003534f12de6b5fa325b32a669057aa7e990a63a
jayanta551905 <jayanta551905@gmail.com>
2023-02-13 00:44:22 +0600

    port update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("./app");
 require("dotenv").config();
 
 // server port listen
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.listen(port, ()=>{
     console.log(`App is running on port ${port}`);

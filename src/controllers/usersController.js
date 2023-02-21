e095d47cea6252dd2aecfb933ac5336d186c8683
jayanta551905 <jayanta551905@gmail.com>
2023-02-21 14:47:45 +0600

    status update

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -5,7 +5,7 @@ exports.registration = (req, res) => {
     let reqBody = req.body;
     usersModel.create(reqBody, (err, data)=>{
         if (err){
-            res.status(400).json({status: 'Fail', data: err});
+            res.status(200).json({status: 'Fail', data: err});
         }
         else{
             res.status(200).json({status: 'Success', data: data});
@@ -14,26 +14,33 @@ exports.registration = (req, res) => {
 }
 
 // user login
-exports.login = (req, res) => {
-    let reqBody = req.body;
-    usersModel.aggregate([
-        {$match:reqBody},
-        {$project:{_id:0,email:1,firstName:1,lastName:1,mobile:1,photo:1}}
-    ], (err, data) =>{
-            if (err){
-                res.status(400).json({status: 'Fail', data: err});
+exports.login=(req,res)=>{
+    let reqBody=req.body
+    let {email, password} = reqBody;
+    if(email&&password){
+        usersModel.aggregate([
+            {$match:reqBody},
+            {$project:{_id:0,email:1,firstName:1,lastName:1,mobile:1,photo:1}}
+        ],(err,data)=>{
+            if(err){
+                res.status(400).json({status:"fail",data:err})
             }
-            else{
-                if (data.length>0){
-                    let Payload={exp: Math.floor(Date.now() / 1000) + (24*60*60), data:data[0]['email']};
-                    let token = jwt.sign(Payload, 'SecretKey123456789');
-                    res.status(200).json({status:"success",token:token,data:data[0]});
+            else {
+                if(data.length>0){
+                    let Payload={exp: Math.floor(Date.now() / 1000) + (24*60*60), data:data[0]['email']}
+                    let token = jwt.sign( Payload,'SecretKey123456789');
+                    res.status(200).json({status:"success",token:token,data:data[0]})
                 }
                 else {
                     res.status(401).json({status:"unauthorized"})
                 }
             }
         })
+    }
+    else{
+        console.log("not found")
+    }
+    
 }
 
 
